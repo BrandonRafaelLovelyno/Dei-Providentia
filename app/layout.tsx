94d35785728da10aaa360eeb3495c5efe56535c8
BrandonRafaelLovelyno <brandonrafael80@gmail.com>
2024-07-19 19:26:30 +0700

    production : first host with cpanel

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ const lexend = Lexend({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dei Providentia",
   description: "Dei Providentia Scholarship Website",
+  icons: {
+    icon: ["/favicon.ico?v=4"],
+    apple: ["apple-touch-icon.png?v=4"],
+    shortcut: ["apple-touch-icon.png"],
+  },
 };
 
 export default function RootLayout({
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  distDir: "_next",
   images: {
     unoptimized: true,
   },
